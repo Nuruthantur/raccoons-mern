90d78d1d864cfa758cb067c4ab2fc7864813b866
Nuruthantur <olivermayer123@gmail.com>
2024-01-29 10:41:35 +0100

    mongoose works, mongodb connected, printing users to html works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,16 @@ import { User } from './@types/users'
 
 function App() {
   const [allUsers, setAllUsers] = useState<User[]>([]);
-  // const [foundUser, setFounduser] = useState<User | null>(null);
+
 
   useEffect(() => {
     const fetchAllUsers = () => {
       fetch("http://localhost:8080/api/users/all")
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
-        const foundUsers = res as User[];
+        console.log("allusers response ", res);
+
+        const foundUsers = res.user as User[];
         setAllUsers(foundUsers)
       })
       .catch(e => console.log(e));
@@ -22,13 +23,11 @@ function App() {
 
   return (
     <div>
-    
-      { allUsers.map( (user) => {
+      { allUsers.map((user) => {
         return <div key={user._id}>
           <p>{user.email}</p>
         </div>
       }) }
-      
     </div>
 
   )
