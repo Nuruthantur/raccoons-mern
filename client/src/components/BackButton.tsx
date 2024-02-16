76882e700c5d877f94d87b2650319432bb519a14
Nuruthantur <olivermayer123@gmail.com>
2024-02-16 16:18:47 +0100

    BackButton added

---
+++ client/src/components/BackButton.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+function BackButton() {
+  const navigateTo = useNavigate();
+
+  const goBack = () => {
+    navigateTo(-1);
+  };
+
+  return (
+    <div>
+      <button className="backButton" onClick={goBack}></button>
+    </div>
+  );
+}
+
+export default BackButton;
